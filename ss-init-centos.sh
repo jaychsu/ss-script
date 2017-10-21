06865c2c4307a18284955474788ae3b72de5c4bb
Jaych Su <hi@jaych.su>
2017-10-21 08:24:03 +0800

    Add script to auto init ssserver

--- ss-init-centos.sh
+++ ss-init-centos.sh
@@ -3,3 +3,9 @@
 # To install shadowsocks
 yum install python-setuptools
 easy_install pip && pip install shadowsocks
+
+# REF: https://github.com/shadowsocks/shadowsocks/wiki/Optimizing-Shadowsocks
+sudo cp ./local.conf /etc/sysctl.d/local.conf
+sysctl --system
+
+nohup ssserver -c /root/ss/ssserver.json -d start &
